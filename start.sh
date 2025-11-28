e30267c35d892645cf7e84e3395a4cfe0b421ef7
VictoWolf <hosalliyash@gmail.com>
2025-11-29 01:55:29 +0530

    start update

--- start.sh
+++ start.sh
@@ -11,6 +11,9 @@ RUN_CONFIG_DIR="$BASE_DIR/config"
 DB_DIR="$BASE_DIR/db"
 KEYS_DIR="$BASE_DIR/keys"
 
+# HARD-CODED CBOR-HEX GENESIS ADDRESS (ALWAYS VALID)
+HARDCODED_GENESIS_HEX="581d602f594c56aaed35e85b1bc4ce2dfab46f21e0c00ac2a9ae7cd27f"
+
 rm -rf "$RAW_CONFIG_DIR" "$RUN_CONFIG_DIR" "$DB_DIR" "$KEYS_DIR"
 mkdir -p "$RAW_CONFIG_DIR" "$RUN_CONFIG_DIR" "$DB_DIR" "$KEYS_DIR"
 
@@ -43,7 +46,7 @@ cp "$RAW_CONFIG_DIR/alonzo-genesis.json" "$RUN_CONFIG_DIR/"
 cp "$RAW_CONFIG_DIR/conway-genesis.json" "$RUN_CONFIG_DIR/"
 
 ############################################################
-# GENERATE PAYMENT KEYPAIR
+# GENERATE PAYMENT KEYPAIR (just for user wallet)
 ############################################################
 
 cardano-cli address key-gen \
@@ -55,14 +58,14 @@ cardano-cli address build \
   --testnet-magic 4 \
   > "$KEYS_DIR/payment.addr"
 
-BECH32_ADDR=$(cat "$KEYS_DIR/payment.addr")
-echo "Funding Address: $BECH32_ADDR"
+USER_ADDR=$(cat "$KEYS_DIR/payment.addr")
+echo "Human Wallet Address: $USER_ADDR"
 
 ############################################################
-# INSERT INITIAL FUNDS (BECH32 ALLOWED ON SANCHONET)
+# INSERT HARD-CODED INITIAL FUNDS
 ############################################################
 
-jq ".initialFunds += {\"$BECH32_ADDR\": {\"lovelace\": 1000000000000}}" \
+jq ".initialFunds += {\"$HARDCODED_GENESIS_HEX\": {\"lovelace\": 1000000000000}}" \
    "$RUN_CONFIG_DIR/shelley-genesis.json" > "$RUN_CONFIG_DIR/tmp.json"
 
 mv "$RUN_CONFIG_DIR/tmp.json" "$RUN_CONFIG_DIR/shelley-genesis.json"
@@ -75,7 +78,7 @@ GENESIS_HASH=$(cardano-cli conway genesis hash \
   --genesis "$RUN_CONFIG_DIR/shelley-genesis.json")
 
 jq ".ShelleyGenesisHash = \"$GENESIS_HASH\"" \
-   "$RUN_CONFIG_DIR/config.json" > "$RUN_CONFIG_DIR/tmp.json"
+  "$RUN_CONFIG_DIR/config.json" > "$RUN_CONFIG_DIR/tmp.json"
 
 mv "$RUN_CONFIG_DIR/tmp.json" "$RUN_CONFIG_DIR/config.json"
 
@@ -94,11 +97,14 @@ cardano-node run \
   --config "$RUN_CONFIG_DIR/config.json" \
   > "$BASE_DIR/node.log" 2>&1 &
 
+sleep 2
+
 echo ""
 echo "==============================="
 echo " PRIVATE SANCHONET NODE READY"
 echo "==============================="
-echo "Address: $BECH32_ADDR"
+echo "User Wallet (Bech32): $USER_ADDR"
+echo "Genesis HEX Funding Address: $HARDCODED_GENESIS_HEX"
 echo "Genesis Hash: $GENESIS_HASH"
 echo "Logs: $BASE_DIR/node.log"
 echo ""
